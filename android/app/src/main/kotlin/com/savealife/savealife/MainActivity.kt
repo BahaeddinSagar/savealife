a000ed5e237e0dfeae5c0c3a30dc8057cc585962
Bahaeddin Sagar <bahasagar@hotmail.com>
2020-09-19 16:04:05 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/savealife/savealife/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.savealife.savealife
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/FirebaseClass.dart
@@ -0,0 +1,68 @@
+import "package:firebase_auth/firebase_auth.dart";
+import "package:cloud_firestore/cloud_firestore.dart";
+
+class FirebaseClass {
+  static final _firestore = Firestore.instance;
+  static final _fireAuth = FirebaseAuth.instance;
+
+  Future<FirebaseUser> login({String email, String password}) async {
+    try {
+      var user = await _fireAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return user.user;
+    } on Exception catch (e) {
+      print(e.toString());
+      print("Authentication error");
+    }
+  }
+
+  Future<FirebaseUser> signup({String email, String password}) async {
+    try {
+      var user = await _fireAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return user.user;
+    } on Exception catch (e) {
+      print(e.toString());
+      print("Authentication error");
+    }
+  }
+
+  void signout() {
+    _fireAuth.signOut();
+  }
+
+  Future<FirebaseUser> getCurrentUser() {
+    return FirebaseAuth.instance.currentUser();
+  }
+
+  static void addEmergency(
+      {String city,
+      String country,
+      String type,
+      String whoIsInjured,
+      GeoPoint location}) {
+    _firestore.collection("emergencies").add({
+      "city": city,
+      "country": country,
+      "type": type,
+      "location": location,
+      whoIsInjured: whoIsInjured
+    }).then((value) {
+      print('done');
+    }).catchError((error) {
+      print(error);
+    });
+  }
+
+  // Map getDocumentFromFirebase({String documentID}) async {
+  //   await for (var snapshot in _firestore.document(documentID).snapshots()) {}
+  // }
+  //
+  // Map getCollectionFromFirestore(String collectionID) async {
+  //   await for (var snapshot in _firestore.collection("messages").snapshots()) {
+  //     for (var document in snapshot.documents) {
+  //       print(document.data);
+  //     }
+  //   }
+  // }
+}
